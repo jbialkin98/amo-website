6193ce5d096505589fbcefe9446c61861f23fdcd
jbialkin98 <jbialkin98@gmail.com>
2023-07-20 16:49:43 -0400

    Separate nav components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { headers } from "next/dist/client/components/headers";
 import React from "react";
+import HorizontalNavHeaders from "./HorizontalNavHeaders";
+import VerticalNavHeaders from "./VerticalNavHeaders";
 import Hamburger from 'hamburger-react'
 import { motion, AnimatePresence } from "framer-motion"
 
@@ -12,12 +14,6 @@ export default function Navbar(props: any) {
     }
 
     const [isOpen, setOpen] = React.useState(false)
-
-    // const [hamburgerClicked, setHamburgerClicked] = React.useState(false);
-
-    // function handleHamburger() {
-    //     setHamburgerClicked(oldHamburgerClicked => !oldHamburgerClicked);
-    // }
 
     const SocialMediaIcons = () => {
         return (
@@ -42,27 +38,6 @@ export default function Navbar(props: any) {
         )
     }
 
-    const NavHeaders = () => {
-        const navArray = ["HOME", "ABOUT", "MEMBERS", "MEDIA", "EVENTS", "CONTACT"];
-        const headers = navArray.map(tab => {
-            return (
-                <a 
-                    className={addUnderline(tab) ? "underline underline-offset-4 hover:scale-110 transition duration-75": 
-                        "hover:scale-110 transition duration-75"}
-                    key={tab}
-                    href={tab !== "HOME" ? `/${tab.toLowerCase()}`: "/"}
-                >
-                    {tab}
-                </a>
-            )
-        })
-        return (
-            <div className={`flex gap-6 text-slate-400 px-5 ${isOpen ? "flex-col items-end relative": "flex"}`}>
-                {headers}
-            </div>
-        )
-    }
-
     return (
         <nav>
             {/* Navbar for large screens */}
@@ -71,50 +46,9 @@ export default function Navbar(props: any) {
                 tracking-wider sticky top-0 z-50 w-screen max-lg:hidden"
             >
                 <a className="text-xl text-black hover:scale-110 transition duration-75" href="/">AMO BRASS</a>
-                {/* Hamburger Icon */}
 
-                {/* <div className="lg:hidden">
-                    <Hamburger 
-                        toggled={isOpen} toggle={setOpen} size={24}
-                        onToggle={toggled => {
-                            if (toggled) {
-                            console.log("Toggled!")
-                            } else {
-                            console.log("Not toggled")
-                            }
-                        }}
-                    />
-                </div> */}
-
-                <NavHeaders />
-                {/* <nav className="flex gap-6 text-slate-400 px-5">
-                    {navHeaders};
-                    <a className="underline underline-offset-4 hover:scale-110 transition duration-75" href="/">HOME</a>
-                    <a href="/about" className="hover:scale-110 transition duration-75">ABOUT</a>
-                    <a href="members.html" className="hover:scale-110 transition duration-75">MEMBERS</a>
-                    <a className="hover:scale-110 transition duration-75">MEDIA</a>
-                    <a className="hover:scale-110 transition duration-75">EVENTS</a>
-                    <a className="hover:scale-110 transition duration-75">CONTACT</a>
-                </nav> */}
+                <HorizontalNavHeaders addUnderline={addUnderline} underline={props.underline}/>
                 <SocialMediaIcons />
-                {/* <div className="flex gap-3">
-                    <a 
-                        onClick={() => props.handleClick('https://www.instagram.com/amobrass')}
-                        className="cursor-pointer h-9 w-9 bg-black rounded-full bg-[url('/icons/instagram.svg')] 
-                        bg-no-repeat bg-center hover:scale-110 transition duration-75"
-                    ></a>
-                    <img 
-                        className="cursor-pointer hover:scale-110 transition duration-75" 
-                        onClick={() => props.handleClick('https://www.facebook.com')}
-                        src="/icons/facebook.svg" 
-                        alt="Facebook Button" 
-                    />
-                    <div 
-                        onClick={() => props.handleClick('https://www.youtube.com/channel/UCS9XQxSYUoXdTld153_sCBw?app=desktop')}
-                        className="cursor-pointer h-9 w-9 bg-black rounded-full bg-[url('/icons/youtube.svg')] bg-no-repeat bg-center 
-                        hover:scale-110 transition duration-75"
-                    ></div>
-                </div> */}
             </div>
                 
             {/* Navbar for small screens */}
@@ -125,7 +59,6 @@ export default function Navbar(props: any) {
                 <a className="text-xl text-black hover:scale-110 transition duration-75" href="/">AMO BRASS</a>
                 
                 <div className="ml-auto">
-                    {/* {isOpen && <NavHeaders />} */}
                     <div>
                         <Hamburger 
                             toggled={isOpen} toggle={setOpen} size={24}
@@ -140,20 +73,16 @@ export default function Navbar(props: any) {
                     </div>
                     
                 </div>
-                
-                    
-                {/* <NavHeaders />
-                <SocialMediaIcons /> */}
             </div>
-                {isOpen && 
-                    <motion.div 
-                        initial={{ y: -250}}
-                        animate={{ y: 0 }} 
-                        className="h-screen border-b-2 flex flex-col items-center px-11 pb-5 gap-5 drop-shadow-2xl"
-                    >
-                        <NavHeaders />
-                        <SocialMediaIcons />                    
-                    </motion.div>}
+            {isOpen && 
+                <motion.div 
+                    initial={{ y: -250}}
+                    animate={{ y: 0 }} 
+                    className="h-screen border-b-2 flex flex-col items-center px-11 pb-5 gap-5 drop-shadow-2xl"
+                >
+                    <VerticalNavHeaders addUnderline={addUnderline} underline={props.underline}/>
+                    <SocialMediaIcons />                    
+                </motion.div>}
         </nav>
     )
 }
---
+++ app/components/VerticalNavHeaders.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function VerticalNavHeaders(props: any) {
+    const navArray = ["HOME", "ABOUT", "MEMBERS", "MEDIA", "EVENTS", "CONTACT"];
+        const headers = navArray.map(tab => {
+            return (
+                <a 
+                    className={props.addUnderline(tab) ? "underline underline-offset-4 hover:scale-110 transition duration-75": 
+                        "hover:scale-110 transition duration-75"}
+                    key={tab}
+                    href={tab !== "HOME" ? `/${tab.toLowerCase()}`: "/"}
+                >
+                    {tab}
+                </a>
+            )
+        })
+        return (
+            <div className={`flex gap-6 text-slate-400 px-5 flex-col items-center"`}>
+                {headers}
+            </div>
+        )
+}

81b5b3274f60d638329c10418be7d92a91a2803c
jbialkin98 <jbialkin98@gmail.com>
2023-08-01 17:47:46 -0400

    Start YouTube media integration

--- app/components/Media.tsx
+++ app/components/Media.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import YouTube from "react-youtube";
 
 export default function Media() {
     return (
         <div>
             <h1>Test</h1>
+            <YouTube 
+                videoId="2g811Eo7K8U" 
+            />
         </div>
     )
 }

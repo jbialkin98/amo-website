def46e83120344719bb39f141ee1e6d80f637b47
jbialkin98 <jbialkin98@gmail.com>
2023-08-08 19:00:21 -0400

    Format contact us

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { DEFAULT_MIN_VERSION } from "tls";
 
 export default function Contact() {
+    const [formSubmitted, setFormSubmitted] = React.useState(false);
+
     async function handleSubmit(event: any) {
         event.preventDefault();
         const formData = new FormData(event.target);
@@ -24,6 +26,7 @@ export default function Contact() {
         const result = await response.json();
         if (result.success) {
             console.log(result);
+            setFormSubmitted(true);
         }
     }
 
@@ -32,25 +35,33 @@ export default function Contact() {
         <div className="h-[43vh] w-screen  bg-[url('/img/avaloch-crop.jpg')] bg-no-repeat bg-cover bg-top flex items-end justify-end">
             <p className="text-white text-7xl font-Josefin pr-8 pb-2">CONTACT</p>
         </div>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <div className="flex flex-col items-center gap-4 m-4 md:flex-row md:items-start">
-            <div>
-                <p>Name:</p>
-                <input type="text" name="name" className="border-2 rounded-md w-60 h-10"/>
-            </div>
-            <div>
-                <p>Email:</p>
-                <input type="email" name="email" className="border-2 rounded-md w-60 h-10"/>
-            </div>
-        </div>
-            <div className="mb-5">
-                <p>Message:</p>
-                <textarea name="message" className="border-2 rounded-md w-60 h-10 md:w-96 md:h-20"></textarea>
+        {!formSubmitted && 
+            <form onSubmit={handleSubmit} className="flex flex-col items-center">
+                <div className="flex flex-col items-center gap-4 m-5 md:flex-row md:items-start">
+                    <div>
+                        <p>Name:</p>
+                        <input type="text" name="name" required className="tx-sm border-2 px-1 rounded-md w-60 h-10"/>
+                    </div>
+                    <div>
+                        <p>Email:</p>
+                        <input type="email" name="email" required className="border-2 rounded-md w-60 h-10"/>
+                    </div>
+                </div>
+                    <div className="mb-5">
+                        <p>Message:</p>
+                        <textarea name="message" required className="border-2 rounded-md w-60 h-10 md:w-96 md:h-20"></textarea>
+                    </div>
+                
+                
+                <button type="submit" className="font-Josefin border rounded-md w-40 h-10 mb-5">Submit</button>
+            </form>
+        }
+        {formSubmitted && 
+            <div className="flex justify-center m-10">
+                <p>Thank you for contacting us!</p>
             </div>
         
-        
-        <button type="submit" className="font-Josefin border rounded-md w-40 h-10 mb-5">Submit Form</button>
-      </form>
+        }
     </div>
   );
 }
